b6e0364de54e594c5ae6ef2cdce089be8535b40f
Afareus <afareus89@gmail.com>
2021-10-18 13:39:35 +0200

    funkční propojení s databází a základní pohledy pro uživatele a auta. Bez vrstvy Repository!

---
+++ AppCarsUsers/AppCarsUsers/Context/CarsUsersContext.cs
@@ -0,0 +1,20 @@
+﻿using AppCarsUsers.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AppCarsUsers.Context {
+    public class CarsUsersContext : DbContext  {
+
+        public CarsUsersContext() : base("CarsUsersConnection") {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Address> Adresses { get; set; }
+       
+
+    }
+}
